cf29a187a411127d73240acda11baccefb8352ca
Sharad1994 <sharadkhandelwal2209@gmail.com>
2022-01-09 23:52:58 +0530

    modifications

--- src/app/card/card.component.ts
+++ src/app/card/card.component.ts
@@ -8,13 +8,38 @@ import { MatDialog } from '@angular/material/dialog';
   styleUrls: ['./card.component.scss']
 })
 export class CardComponent implements OnInit {
-  
+
   constructor(public dialog: MatDialog) { }
+
+  cardList = [
+    {
+      url: "https://material.angular.io/components/card/examples",
+      name: "Jira 1",
+      image: "../../assets/images/Jira.png"
+    },
+    {
+      url: "https://material.angular.io/components/card/examples",
+      name: "Jira 2",
+      image: "../../assets/images/Jira.png"
+    }, {
+      url: "https://material.angular.io/components/card/examples",
+      name: "Jira 3",
+      image: "../../assets/images/Jira.png"
+    }, {
+      url: "https://material.angular.io/components/card/examples",
+      name: "Jira 4",
+      image: "../../assets/images/Jira.png"
+    }, {
+      url: "https://material.angular.io/components/card/examples",
+      name: "Jira 5",
+      image: "../../assets/images/Jira.png"
+    }
+  ]
 
   ngOnInit(): void {
   }
 
-  openWidgetModal(){
+  openWidgetModal() {
     const dialogRef = this.dialog.open(ModalWidgetComponent, {
       width: '25%',
       disableClose: true,
