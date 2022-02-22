79b8af6024bb9e3e7bdba506087b4fe439fa3e3d
optimus1113700 <91974225+optimus1113700@users.noreply.github.com>
2022-02-22 20:03:00 +0530

    Update header.component.ts

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -2,6 +2,17 @@ import { Component, OnInit } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { TemplateFormComponent } from '../template-form/template-form.component';
 
+export interface Tile {
+  tileName?: string;
+  link?: string;
+  rows?: number;
+  image?: any;
+  cols?: number;
+  details?: string;
+  color?: string;
+  text?: string
+}
+
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
@@ -14,15 +25,22 @@ export class HeaderComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  tiles: Tile[] = [
+    { tileName: 'Jira', link: 'https://jira.com', rows: 1, image: 'assets/images/Jira.png', cols: 3, details: 'Jira Software is part of a family of products designed to help teams of all types manage work. Originally, Jira was designed as a bug and issue tracker.' },
+    { tileName: 'Git', link: 'https://gmail.com', rows: 2, image: 'https://blogcdn.gmass.co/blog/wp-content/uploads/2020/04/Featured-image-hosted-vs-embedded-images-104kb.png', cols: 1, details: 'Git is a free and open source distributed version control system .' },
+    { tileName: 'Gmail', link: 'https://stash.com', rows: 1, image: 'https://cdn-media-1.freecodecamp.org/images/VQhi-KgyeBh6jegrDc2zaLOGxsBWq0Bw5dNq', cols: 1, details: 'Gmail is a free email service provided by Google' },
+    { tileName: 'Jira', link: 'https://jira.com', rows: 1, image: 'assets/images/Jira.png', cols: 2, details: 'Jira Software is part of a family of products designed to help teams of all types manage work. Originally, Jira was designed as a bug and issue tracker.' },
+  ]
+
   openTemplateForm() {
     const dialogRef = this.dialog.open(TemplateFormComponent, {
       disableClose: true,
       data: {},
     });
 
-    // dialogRef.afterClosed().subscribe(result => {
-    //   console.log('The dialog was closed');
-    // });
+    dialogRef.afterClosed().subscribe(result => {
+      this.tiles.push(result);
+    });
   }
 
 }
